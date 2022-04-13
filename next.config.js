5ea8cba8da01c9ab490121b9e072482288e6b33a
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-13 03:15:00 -0400

    lint errors, changed extend eslint for react, updated lint scripts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  serverRuntimeConfig: {
-    PROJECT_ROOT: __dirname
-  }
+	reactStrictMode: false,
+	serverRuntimeConfig: {
+		PROJECT_ROOT: __dirname
+	}
 }
 
 module.exports = nextConfig
--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,17 +1,17 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const components = {
-  Button: {
-    baseStyle: {
-      _focus: {
-        boxShadow: 'none'
-      }
-    }
-  }
+	Button: {
+		baseStyle: {
+			_focus: {
+				boxShadow: 'none'
+			}
+		}
+	}
 }
 
 export const theme = extendTheme({
-  initialColorMode: 'light',
-  useSystemColorMode: true,
-  components
+	initialColorMode: 'light',
+	useSystemColorMode: true,
+	components
 })
