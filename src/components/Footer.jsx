b104d11c17269557ad65aeeaf2ee51458ba16655
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-22 18:41:04 -0400

    lint

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,41 @@
 import {
-	Flex,
-	chakra,
-	useColorModeValue
+  Flex,
+  chakra,
+  useColorModeValue
 } from '@chakra-ui/react'
 import Link from '@components/Link'
 import VercelIcon from '@components/VercelIcon'
 
 const Footer = ({ ...props }) => {
-	return (
-		<chakra.footer
-			as='footer'
-			display='flex'
-			flex={1}
-			py={4}
-			borderTop='1px solid #eaeaea'
-			justifyContent='center'
-			alignItems='center'
-			flexDir='row'
-			{...props}
-		>
-			<Flex justifyContent='center' alignItems='center'>
-				<Link
-					isExternal
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-				>
-					Powered by{' '}
-					<chakra.span ml={2} display='inline-block'>
-						<VercelIcon
-							fill={useColorModeValue('#000', '#fff')}
-							width='18'
-							height='4'
-						/>
-					</chakra.span>
-				</Link>
-			</Flex>
-		</chakra.footer>
-	)
+  return (
+    <chakra.footer
+      as='footer'
+      display='flex'
+      flex={1}
+      py={4}
+      borderTop='1px solid #eaeaea'
+      justifyContent='center'
+      alignItems='center'
+      flexDir='row'
+      {...props}
+    >
+      <Flex justifyContent='center' alignItems='center'>
+        <Link
+          isExternal
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+        >
+          Powered by{' '}
+          <chakra.span ml={2} display='inline-block'>
+            <VercelIcon
+              fill={useColorModeValue('#000', '#fff')}
+              width='18'
+              height='4'
+            />
+          </chakra.span>
+        </Link>
+      </Flex>
+    </chakra.footer>
+  )
 }
 
 export default Footer
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,11 +3,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '@lib/theme'
 
 function MyApp ({ Component, pageProps }) {
-	return (
-		<ChakraProvider theme={theme}>
-			<Component {...pageProps}/>
-		</ChakraProvider>
-	)
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps}/>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
