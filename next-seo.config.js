add281c39121b0683458b749316f33c370e35986
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-08 18:54:37 -0400

    description updated

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,7 +1,7 @@
 /* @ref: https://github.com/garmeeh/next-seo */
 
 const title = 'Next JS + Chakra UI Starter'
-const description = 'Next JS + Chakra UI Starter - A boilerplate for React app using Next.js and Chakra UI.'
+const description = 'Next JS + Chakra UI Starter - A boilerplate for React apps using Next.js and Chakra UI.'
 const url = 'https://next-chakra-starter-pi.vercel.app'
 const imageUrl = `https://og-image.vercel.app/${encodeURI(title)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`
 const keywords = 'nextjs, chakra-ui, react, javascript, boilerplate, next-seo'
