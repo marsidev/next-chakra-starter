0abdf035da283f21e508af6149200a1ad99cbce8
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-08 21:15:26 -0400

    feat(components): :lipstick: change footer ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,63 @@
 import {
   Flex,
-  chakra,
+  HStack,
+  Icon,
+  Switch,
+  Text,
+  useColorMode,
   useColorModeValue
 } from '@chakra-ui/react'
+import { BsSunFill as SunIcon, BsMoonFill as MoonIcon } from 'react-icons/bs'
 import Link from '@components/Link'
-import VercelIcon from '@components/VercelIcon'
+
+const GITHUB_URL = 'https://github.com/marsigliadev'
 
 const Footer = ({ ...props }) => {
+  const { toggleColorMode } = useColorMode()
+
   return (
-    <chakra.footer
+    <Flex
       as='footer'
-      display='flex'
-      flex={1}
-      py={4}
-      borderTop='1px solid #eaeaea'
-      justifyContent='center'
-      alignItems='center'
-      flexDir='row'
+      bg={useColorModeValue('white', 'gray.800')}
+      color={useColorModeValue('gray.700', 'gray.200')}
+      borderTop='1px solid'
+      borderColor={useColorModeValue('gray.200', 'gray.700')}
+      width='100%'
+      h='auto'
+      justify='center'
+      align='center'
       {...props}
     >
-      <Flex justifyContent='center' alignItems='center'>
-        <Link
-          isExternal
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+      <Flex h='full' w='100%' maxW='5xl'>
+        <Flex
+          justify='space-between'
+          align='center'
+          w='100%'
+          h='auto'
+          flexDir={{ base: 'column', md: 'row' }}
+          gap={{ base: 2, md: 0 }}
         >
-          Powered by{' '}
-          <chakra.span ml={2} display='inline-block'>
-            <VercelIcon
-              fill={useColorModeValue('#000', '#fff')}
-              width='18'
-              height='4'
-            />
-          </chakra.span>
-        </Link>
+          <Text color={useColorModeValue('#8e8e8e', 'gray.500')} fontSize={16}>
+            {'Made with ♥ by '}
+            <Link
+              href={GITHUB_URL}
+              color={useColorModeValue('blue.600', 'blue.300')}
+              isExternal
+              textDecoration='underline'
+            >
+              Luis Marsiglia
+            </Link>
+            {'.'}
+          </Text>
+
+          <HStack justify='center'>
+            <Icon as={SunIcon} w={4} h={4} />
+            <Switch colorScheme='teal' size='md' onChange={toggleColorMode} />
+            <Icon as={MoonIcon} w={4} h={4} />
+          </HStack>
+        </Flex>
       </Flex>
-    </chakra.footer>
+    </Flex>
   )
 }
 
