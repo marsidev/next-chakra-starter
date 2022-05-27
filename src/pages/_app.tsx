68a6ae6da6b39f06d4a3d3baf07ef7df09c39119
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-27 17:03:43 -0400

    perf: :zap: improve chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import { ChakraProvider, ScaleFade } from '@chakra-ui/react'
-import { theme } from '@lib/theme'
+import { ScaleFade } from '@chakra-ui/react'
+import { ChakraProvider } from '@lib/Chakra'
 import Layout from '@layouts/main'
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider cookies={pageProps.cookies}>
       <Layout>
         <ScaleFade key={router.route} in={true} initialScale={0.85}>
           <Component {...pageProps} />
